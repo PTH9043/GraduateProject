51a261ac51c5c85272f591c06b52ea76318b2d88
Park Tae Heon <my5827@naver.com>
2024-04-30 00:07:08 +0900

    GitUpdate

--- CodeDungeon/Engine/private/UPlayerController.cpp
+++ CodeDungeon/Engine/private/UPlayerController.cpp
@@ -1,15 +1,23 @@
 #include "EngineDefine.h"
 #include "UPlayerController.h"
 
-UPlayerController::UPlayerController(CSHPTRREF<UDevice> _spDevice)
-	: UController(_spDevice)
+
+UPlayerController::UPlayerController(CSHPTRREF<UDevice> _spDevice, const _int _iKeyLength)
+	: UController(_spDevice), m_iKeyLength{ _iKeyLength }
 {
 }
 
 UPlayerController::UPlayerController(const UPlayerController& _rhs) :
-	UController(_rhs)
+	UController(_rhs), m_iKeyLength{_rhs.m_iKeyLength}
 {
+	m_PressingKeyContainer.resize(m_iKeyLength);
+	m_UpKeyContainer.resize(m_iKeyLength);
+	m_DownKeyContainer.resize(m_iKeyLength);
 }
+
+#pragma region STRUCT 
+
+#pragma endregion STRUCT
 
 void UPlayerController::Free()
 {
@@ -17,11 +25,16 @@ void UPlayerController::Free()
 
 HRESULT UPlayerController::NativeConstruct()
 {
-	return S_OK;
+	return __super::NativeConstruct();
 }
 
 HRESULT UPlayerController::NativeConstructClone(const VOIDDATAS& _tDatas)
 {
+	if (FAILED(__super::NativeConstructClone(_tDatas)))
+		return E_FAIL;
+
+
+
 	return S_OK;
 }
 
@@ -32,4 +45,8 @@ void UPlayerController::Tick(UCharacter* _pCharacter, const _double& _dTimeDelta
 _bool UPlayerController::InputTriggerData(UCharacter* _pCharacter, const _int _Data)
 {
 	return _bool();
+}
+
+void UPlayerController::CheckInput(const INPUTDESC _InputDesc)
+{
 }
